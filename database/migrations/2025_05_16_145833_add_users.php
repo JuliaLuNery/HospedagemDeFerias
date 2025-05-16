85c2f23bff480c70a6082eef06f69d4185698796
Júlia Nery <julia_luizanery@hotmail.com>
2025-05-16 16:51:37 +0100

    atualização e-mail e tabelas user e locações

---
+++ database/migrations/2025_05_16_145833_add_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use function Laravel\Prompts\table;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('sobrenome')->nullable()->after('name');
+            $table->string('nif')->nullable()->after('email');
+            $table->string('telefone')->unique()->after('nif');
+            $table->date('morada')->nullable()->after('telefone');
+            $table->text('data_nascimento')->nullable()->after('morada');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+             $table->dropColumn([
+                'sobrenome','telefone', 'nif', 'data_nascimento', 'morada'
+            ]);
+        });
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MailController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,5 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::post('/enviar-mail',[MailController::class, 'sendReservationEmail'] )
+->middleware('auth')
+->name('send.email');
 
 require __DIR__.'/auth.php';
